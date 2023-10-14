10b4acbbaaf1c1be58c59080d86db1c29127cd9c
Miranda Ubong <136003350+MirandaUbong@users.noreply.github.com>
2023-10-14 18:21:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ import time
 
 
 header = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 "
-                  "Safari/537.36",
+    "User-Agent": "MY AGENT",
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
 }
 
